2360ff50f5ebc9e88fef341afea3f7b3bc948374
Mujtaba-git <mujtaba.links@gmail.com>
2023-04-20 14:59:43 +0500

    new

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,8 +6,8 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    def wait_for_element(self, locator, timeout=30):
+    def wait_for_element(self, locator, timeout=100):
         WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
-    def wait_and_click(self, locator, timeout=30):
+    def wait_and_click(self, locator, timeout=100):
         WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator)).click()
